3b27e51f6be3ec4d4198ba2c700cbb155cd9dca7
Catherine Washburn <cawashburn@gmail.com>
2016-06-01 23:10:30 -0400

    Created project

---
+++ BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Catherine Washburn on 6/1/16.
+//  Copyright © 2016 Catherine Washburn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
